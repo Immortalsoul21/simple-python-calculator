316823cd3ecf51c89b493f7b76343f7d72c4921e
Tushar Mondal <129579596+Immortalsoul21@users.noreply.github.com>
2023-04-13 10:21:49 +0000

    added sub , mul ,  div ,clear button

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import *
-def add():
-  num1=int(t1.get())
-  num2=int(t2.get())
-  sum = num1 + num2
-  t3.insert(END,str(sum))
+
+
+
+
+
+
+
+
 
 window = Tk()
 window.geometry("400x300")
@@ -27,9 +30,50 @@ labl3.place(x=70,y=140)
 t3= Entry()
 t3.place(x=170,y=140)
 
-b1 = Button(window,text="Add",command=add)
-b1.place(x=60,y=180)
+def add():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum = num1 + num2
+  t3.insert(END,str(sum))
 
+b1 = Button(window,text="Add",command=add)
+b1.place(x=50,y=200)
+
+def sub():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  dif = num1 - num2
+  t3.insert(END,str(dif))
+
+b2 = Button(window,text="Sub",command=sub)
+b2.place(x=120,y=200)
+
+def multiply():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  product = num1 * num2
+  t3.insert(END,str(product))
+b3 = Button(window,text="Multiply",command=multiply)
+b3.place(x=220,y=200)
+
+
+def div():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  div = num1 / num2
+  t3.insert(END,str(div))
+
+
+b4 = Button(window,text="Divide",command=div)
+b4.place(x=320,y=200)
+
+def clear():
+  t1.delete(0,END)
+  t2.delete(0,END)
+  t3.delete(0,End)
+
+b5 = Button(window,text="Clear",command=clear)
+b5.place(x=180,y=250)
 
    
 
