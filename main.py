aa311548183aeae64567b784b4062358e2a79674
Tushar Mondal <129579596+Immortalsoul21@users.noreply.github.com>
2023-04-13 09:36:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("300x300")
+window.title("simple calculator")
+
+labl1 = Label(window,text="Enter 1st Number:")
+labl1.place(x=50,y=50)
+
+labl2 = Label(window,text="Enter 2st Number:")
+labl2.place(x=50,y=100)
